d3aae63c0862ed5287b2c0f8be40448fb609f869
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-03-15 18:15:34 +0400

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,15 @@
+name = "Atharv"
+age = 10
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type of name is", type(name))
+print("age :" , age)
+print("Data Type of age is", type(age))
+print("is_student :" , (is_student))
+print("Data Type of is_student is", type(is_student))
+print("weight :" , type(weight))
+print("\n After Type Casting....")
+age = str(age)
+print(age)
