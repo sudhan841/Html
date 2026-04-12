e30bf487dee2cead9c960d2a55d9a93a06cc3e16
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-04-12 20:08:27 +0400

    Add files via upload

---
+++ square.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+ 
+# Square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+
+#Triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+turtle.done()
