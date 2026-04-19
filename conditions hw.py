cd7235c5ed8ae2bd42561774eadf8729910806c4
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-04-19 19:56:59 +0400

    Add files via upload

---
+++ conditions hw.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Number to Check"))
+print("Number to be checked :", number) 
+if number>0:
+ print("This is a positive number")
+elif number<0:
+ print ("This is a negative number")
+
+else:
+ print("This is neutral")
