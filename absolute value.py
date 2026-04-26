79eba606ba372fed5b847569a10696a0175478b1
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-04-26 17:56:23 +0400

    Add files via upload

---
+++ absolute value.py
@@ -0,0 +1,11 @@
+def absolute_value(num):
+    
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+
+print("Absolute Value of 89 :", absolute_value(89))
+
+print("Absolute Value of -189 :", absolute_value(-189))
