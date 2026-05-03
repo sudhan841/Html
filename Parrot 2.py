f345bae05e027291a82522af5e0d9a1b2c2b63f1
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-05-03 18:12:50 +0400

    Add files via upload

---
+++ Parrot 2.py
@@ -0,0 +1,11 @@
+class Parrot:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sing(self, song):
+            return "{} sings {}".format(self.name, song)
+    
+    def dance(self):
+         return "{} is now dancing"
