8aad25bec5aed24c62e62ff429664f9f8e0ac8f0
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-05-07 14:22:57 +0400

    Add files via upload

---
+++ bird.py
@@ -0,0 +1,11 @@
+class cat:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sing(self, song):
+            return "{} sings {}".format(self.name, song)
+    
+    def dance(self):
+         return "{} Cat is now dancing"
