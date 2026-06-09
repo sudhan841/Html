8a7f77c16f317f232df8494a8d64db0c82b87651
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-06-09 11:32:06 +0400

    Add files via upload

--- Phone book.py
+++ Phone book.py
@@ -19,7 +19,33 @@ def create_phonebook():
         pb.append([name, number, email, dob, category])
 
     return pb
+def display_contacts(pb):
+    if not pb:
+        print("\nPhonebook is empty!")
+        return
 
+    print("\n_ _ _ CONTACT LIST _ _ _ ")
+    print("{:<15} {:<15} {:<25} {:<15} {:<10}".format("Name", "Number", "Email", "DOB", "Category"))
+
+    for contact in pb:
+        print("{:<15} {:<15} {:<25} {:<15} {:<10}".format(
+            contact[0], contact[1], contact[2], contact[3], contact[4]))
+def add_contact(pb):
+        print("\nEnter new contact details")
+
+        name = input("Name: ")
+        number = input("Phone Number: ")
+        email = input("Email: ")
+        dob = input("DOB: ")
+        category = input("Category: ")
+
+        pb.append([name, number, email, dob, category])
+
+        print("Contact added successfully!")
+def search_contact(pb):
+             name = input("\nEnter name to search: ")
+             
+            found = False
 def menu():
     print("\n1.Add Contact")
     print("2.Remove Contact")
