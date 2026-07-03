6726980b3764f6d44622961b8ab67e439949a6a6
sudhan841 <83488559+sudhan841@users.noreply.github.com>
2026-07-03 10:17:57 +0400

    Add files via upload

---
+++ dice simulator.py
@@ -0,0 +1,28 @@
+import pygame, random
+
+pygame.init()
+
+screen = pygame.display.set_mode((960,640))
+pygame.display.set_caption("Rock Paper Scissors")
+
+bg = pygame.image.load("Game.jpng")
+
+rock_btn = pygame.image.load("Rock button.jpng")
+paper_btn = pygame.image.load("Paper button.jpng")
+scissor_btn = pygame.image.load("Scissor buttons.jpng")
+
+rock = pygame.image.load("Rock")
+import pygame, random
+
+pygame.init()
+
+screen = pygame.display.set_mode((960,640))
+pygame.display.set_caption("Rock Paper Scissors")
+
+bg = pygame.image.load("Game.jpng")
+
+rock_btn = pygame.image.load("Rock button.jpng")
+paper_btn = pygame.image.load("Paper button.jpng")
+scissor_btn = pygame.image.load("Scissor buttons.jpng")
+
+rock = pygame.image.load("Rock")
